47701d6e36ba84c80220c3ce7f1753da180925c7
lucascr <lcrodrigues1995@gmail.com>
2022-10-26 14:40:27 -0300

    Write tests for Posts

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,33 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:all) do
+      get user_posts_path(user_id: 1)
+    end
+
+    it 'returns http success' do
+      get '/users/show/posts/'
+      puts response
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'GET /show' do
+    before(:each) do
+      get user_post_path(user_id: 1, id: 1)
+    end
+
+    it 'returns http success' do
+      get '/users/show/posts/show'
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns http success' do
+      expect(response).to render_template(:show)
+    end
+
+    it 'Render correct placeholder text' do
+      expect(response.body).to include('Here is a specific post for specific user')
+    end
   end
 end
