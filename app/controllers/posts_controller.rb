4bf73ef95b4c48a1e66864ee45cd3321b4f39dbe
lucascr <lcrodrigues1995@gmail.com>
2022-10-27 20:36:01 -0300

    Update posts_controller.rb
    To allow for creation of new posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,27 @@ class PostsController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:user_id])
-    @post = Post.find(params[:id])
+    @post = Post.includes(:comments, :author).find_by(author_id: params[:user_id], id: params[:id])
+    @user = current_user
+  end
+
+  def new
+    @post = Post.new
+    @user = current_user
+  end
+
+  def create
+    parameters = post_params
+    post = Post.new(author_id: current_user.id, title: parameters[:title], text: parameters[:text])
+    post.save
+    if post.save
+      redirect_to user_path(current_user)
+    else
+      redirect_to new_user_post_path
+    end
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
