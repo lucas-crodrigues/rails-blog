93b668e4a108120d8f9abbd18d0a4c0cf4202373
lucascr <lcrodrigues1995@gmail.com>
2022-11-04 15:00:59 -0300

    Add is? method on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,9 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
-  has_many :comments, foreign_key: 'author_id'
-  has_many :posts, foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
+  has_many :comments, foreign_key: 'author_id', dependent: :destroy
+  has_many :posts, foreign_key: 'author_id', dependent: :destroy
+  has_many :likes, foreign_key: 'author_id', dependent: :destroy
 
   validates :name, presence: true
   validates :posts_counter, comparison: { greater_than_or_equal_to: 0 }, numericality: { only_integer: true }
@@ -16,5 +16,11 @@ class User < ApplicationRecord
 
   def all_posts
     posts.order('created_at Desc')
+  end
+  
+  Roles = [ :admin , :default ]
+
+  def is?(requested_role)
+    self.role = requested_role.to_s
   end
 end
